f32f37abdfc1ad14ddb0a538da118134b9dc1bab
R H <165597973+rhuda21@users.noreply.github.com>
2025-01-03 11:52:03 +0000

    Create WebhookFunction.lua

---
+++ WebhookFunction.lua
@@ -0,0 +1,17 @@
+local function SendMessageEMBED(url, embed, mention)
+    local response = request({
+        Url = url,
+        Method = "POST",
+        Headers = {["Content-Type"] = "application/json"},
+        Body = http:JSONEncode({
+            content = mention and ("<@" .. mention .. ">") or nil,
+            embeds = {{
+                title = embed.title,
+                description = embed.description,
+                color = embed.color,
+                fields = embed.fields,
+                footer = {text = embed.footer.text}
+            }}
+        })
+    })
+end
