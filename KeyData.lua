75f262d9b95cfd17cea7cb094bf0b03b0a130dbf
R H <165597973+rhuda21@users.noreply.github.com>
2025-01-03 12:15:37 +0000

    Update KeyData.lua

--- KeyData.lua
+++ KeyData.lua
@@ -1,6 +1,13 @@
 local Keys {
-  ["1"] = {
+  [1] = {
+    Item = "UltraScripts";
     Enabled = true;
     Active = true;
     Expire = 0;
+  },
+  [2] = {
+    Item = "climbing";
+    Enabled = false;
+    Active = true;
+    Expire = 1;
   },
